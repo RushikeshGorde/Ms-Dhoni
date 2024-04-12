0e0cf6485960a8eaff9f51bfd49d3b01163a005e
RushikeshGorde <134618502+RushikeshGorde@users.noreply.github.com>
2024-04-12 21:47:31 +0530

    For Dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './Components/All/Home'
+import About from './Components/All/About'
+import Trophy from './Components/All/Trophy'
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Layout />}>
+              <Route path='' element={<Home />}/>
+              <Route path='/about' element={<About />}/>
+              <Route path='/trophy' element={<Trophy />}/>
+        </Route>
+    )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    <RouterProvider router={router}/>
 
 )
