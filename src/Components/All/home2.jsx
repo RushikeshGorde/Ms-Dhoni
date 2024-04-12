b02056b0390a691940481ccecea87bb32ba279fa
RushikeshGorde <134618502+RushikeshGorde@users.noreply.github.com>
2024-04-12 18:51:05 +0530

    Create home2.jsx

---
+++ src/Components/All/home2.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from '../Nav/Nav'
+import About from './About'
+import Foot from '../Footer/Foot'
+
+const home2 = () => {
+  return (
+    <>
+
+  <Nav/>
+  <About/>
+  <Foot/>
+    </>
+  )
+}
+
+export default home2
